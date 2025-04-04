b364821688396b432a0b38eea270e9ee1584f797
Kgothatso Maisa <123masiakgothatso@gmail.com>
2025-04-05 00:39:49 +0200

    edit subscription controller file

--- controllers/subcription.controller.js
+++ controllers/subcription.controller.js
@@ -1,6 +1,6 @@
 import Subscription from "../models/subscription.model.js";
 
-export const createSubscription = async (req, resizeBy, next) => {
+export const createSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.create({
             ...req.body,
